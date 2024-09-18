7b99f64e1ca06c8d3b6a6a1557aa964643fb1187
Evgenya888 <e.v.a240967@gmail.com>
2024-09-18 14:03:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+Name = 'Python'
+time_per_task = 1.5/12
+print('Курс:', Name,  ',всего задач ', number_of_homework_completed,',затрачено часов', number_of_hours_spent,
+      ',среднее время', time_per_task, )
